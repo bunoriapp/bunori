bc191283ddfd36ae3f8d9969a68e4128de152914
Binit06 <binitlenka@gmail.com>
2026-09-22 16:18:06 +0530

    fix: Fixed cover image file location not getting saved in the database

--- app/src/main/java/com/halovoid/bunori/data/repository/NovelRepository.kt
+++ app/src/main/java/com/halovoid/bunori/data/repository/NovelRepository.kt
@@ -86,7 +86,7 @@ class NovelRepositoryImpl private constructor(context: Context) : NovelRepositor
             title = if (novel.title.isNotBlank()) novel.title else (existing?.title ?: novel.title),
             author = novel.author?.takeIf { it.isNotBlank() } ?: existing?.author,
             description = novel.description?.takeIf { it.isNotBlank() } ?: existing?.description,
-            coverUrl = existing?.coverUrl ?: novel.coverUrl,
+            coverUrl = novel.coverUrl ?: existing?.coverUrl,
             coverHttpsUrl = novel.coverHttpsUrl ?: existing?.coverHttpsUrl,
             crawlerName = novel.crawlerName.ifBlank { existing?.crawlerName ?: "" },
             inLibrary = inLibrary,
